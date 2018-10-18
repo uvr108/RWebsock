1dfd1afbf99f37870f5394d3a695490f6391de2c
Ulises Vergara <ulises108@gmail.com>
2018-10-18 12:04:10 -0300

    Se agrega flag preliminar

--- collector.py
+++ collector.py
@@ -2,7 +2,7 @@ import socketserver
 import rethinkdb as r
 import os
 import json
-import logging
+#import logging
 import asyncio
 
 from rthkdb.continuos import Continuos
@@ -16,44 +16,52 @@ conn = r.connect(host=rt_host, port=rt_port, db=rt_db)
 
 async def collector(r,data):
 
-    logging.basicConfig(level=logging.DEBUG, filename="/home/ulises/RWebsock/logfile", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+    # logging.basicConfig(level=logging.DEBUG, filename="/home/ulises/RWebsock/logfile", filemode="a+",
+    #                    format="%(asctime)-15s %(levelname)-8s %(message)s")
     #print("COLLECTOR : ",data)
 
-    logging.info('COLLECTOR %s' % data)
+    # logging.info('COLLECTOR %s' % data)
 
     bdata = data.decode('utf8').split('|') 
     #print("DBDATA : ",bdata)
+
+    if dbdata[0] == 'version':
      
-    sfile = bdata[0]
-    yr = bdata[1]
-    mo = bdata[2]
-    tipo = bdata[3]
-    email = bdata[4]
-    delay = bdata[5]
-    sensible = bdata[6]
-   
-    #print("EMAIL : ",email) 
- 
-    if sensible == 'no':
-        if email == 'no' and sensible == 'no':
-            kw={'table':'triggers','get_all':{'flag':'count','where': {'sfile': sfile },'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}};
+        sfile = bdata[1]
+        yr = bdata[2]
+        mo = bdata[3]
+        tipo = bdata[4]
+        email = bdata[5]
+        delay = bdata[6]
+        sensible = bdata[7]
 
-        elif email == 'yes' and sensible == 'no':
-            where = {'sfile': sfile,'tipo_estadistica':tipo }
+        if sensible == 'no':
+            if email == 'no' and sensible == 'no':
+                kw={'table':'triggers','get_all':{'flag':'count','where': {'sfile': sfile },'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}};
 
-            if tipo == 'final':
+            elif email == 'yes' and sensible == 'no':
+                where = {'sfile': sfile,'tipo_estadistica':tipo }
 
-                where.update({'pup':'yes'})
-            kw={'table':'triggers','update':{'setea':{'email_origen': delay}, 'where': where,'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}};
+                if tipo == 'final':
 
-    elif sensible == 'yes':
+                    where.update({'pup':'yes'})
+                kw={'table':'triggers','update':{'setea':{'email_origen': delay}, 'where': where,'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}};
 
-        kw={'table':'triggers','update':{'setea':{'sensible': sensible}, 'where': { 'sfile': sfile,'tipo_estadistica': 'final', 'pup' : 'yes' },'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}}
+        elif sensible == 'yes':
 
-    logging.info(kw)
+            kw={'table':'triggers','update':{'setea':{'sensible': 'yes'}, 'where': { 'sfile': sfile,'tipo_estadistica': 'final', 'pup' : 'yes' },'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}}
+
+
+    elif dbdata[0] == 'preliminar':
+
+        nombre = dbdata[1]
+        yr = bdata[2]
+        mo = bdata[3]
+        
+        kw={'table':'triggers','get_all':{'flag':'count','where': {'sfile': nombre,'tipo_estadistica':'preliminar' },'indice':{'anomes':{'ano_sfile': int(yr), 'mes_sfile': int(mo)}}}};
 
     cons = Continuos()
+
     cons.ejecutar(r,**kw)
     output=cons.output
     cons.__del__()
