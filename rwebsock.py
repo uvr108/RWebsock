9e4c2b28ead116f29b6a77b5fb74051d073da794
osito <uvergara@csn.uchile.cl>
2019-06-03 17:38:00 -0400

    index,betweeb,betweenISO

--- rwebsock.py
+++ rwebsock.py
@@ -18,24 +18,27 @@ class RWebsock(socketserver.StreamRequestHandler):
         print("{} wrote:".format(self.client_address[0]))
         # Likewise, self.wfile is a file-like object used to write back
         # to the client
-        
+       
+ 
         if self.data == b'':
             pass
         else: 
             self.wfile.write(b'Que grande eres Uli !!!')
             vector =  self.data.decode('utf8').split('|')
+        
+            print(vector)
             
             if vector[0] == 'ingresar':
 
                 dictio = {'version': vector[1], 'epoch':vector[2],'action':vector[3],'operator':vector[4]}
                 dictio.update({'retardo':vector[5],'email_origen':vector[6],'sensible':vector[7],'sfile':vector[8]})
                 dictio.update({'tipo_estadistica':vector[9],'latitud':vector[10],'longitud':vector[11],'dep':vector[12],'m1_magnitud':vector[13],'m1_tipo':vector[14]})
-                dictio.update({'fecha_origen':vector[15],'pup':vector[16],'m5':vector[17],'m20':vector[18]})
-                dictio.update({'origen':vector[19],'no':vector[20]})
+                dictio.update({'fecha_origen':vector[15],'ano_sfile':vector[16],'mes_sfile':vector[17],'dia_sfile':vector[18],'pup':vector[19],'m5':vector[20],'m20':vector[21]})
+                dictio.update({'origen':vector[22],'no':vector[23]})
 
             elif vector[0] == 'modificar':
 
-                dictio = {'sf':vector[1],'tipo':vector[2],'email':vector[3],'delay':vector[4],'sensible':vector[5]}
+                dictio = {'sf':vector[1], 'yr':vector[2], 'mo':vector[3], 'tipo':vector[4], 'email':vector[5], 'delay':vector[6], 'sensible':vector[7]} 
             
             asyncio.run(ejecuta(vector[0], {vector[0]: dictio}))
 
