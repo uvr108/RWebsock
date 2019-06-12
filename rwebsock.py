74d357353261d454e72d819f04c1eeeacb4e9220
osito <uvergara@csn.uchile.cl>
2019-06-12 17:23:28 -0400

    Cambios

--- rwebsock.py
+++ rwebsock.py
@@ -26,14 +26,13 @@ class RWebsock(socketserver.StreamRequestHandler):
             self.wfile.write(b'Que grande eres Uli !!!')
             vector =  self.data.decode('utf8').split('|')
         
-            print(f'vector: {vector} ')
             
             if vector[0] == 'ingresar':
 
                 dictio = {'version': vector[1], 'epoch':vector[2],'action':vector[3],'operator':vector[4]}
                 dictio.update({'retardo':vector[5],'email_origen':vector[6],'sensible':vector[7],'sfile':vector[8]})
                 dictio.update({'tipo_estadistica':vector[9],'latitud':vector[10],'longitud':vector[11],'dep':vector[12],'m1_magnitud':vector[13],'m1_tipo':vector[14]})
-                dictio.update({'fecha_origen':vector[15],'ano_sfile':vector[16],'mes_sfile':vector[17],'dia_sfile':vector[18],'pup':vector[19],'m5':vector[20],'m20':vector[21]})
+                dictio.update({'fecha_origen':vector[15],'ano_sfile':vector[16],'mes_sfile':vector[17],'dia_sfile':vector[18],'up':vector[19],'m5':vector[20],'m20':vector[21]})
                 dictio.update({'origen':vector[22],'no':vector[23]})
 
             elif vector[0] == 'modificar':
